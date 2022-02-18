f91a130142fb696861f04b01cce21d58e83ce6c7
shaggysquirrell <40532678+shaggysquirrell@users.noreply.github.com>
2022-02-17 23:30:16 -0500

    If world elif house

--- MainGame.py
+++ MainGame.py
@@ -11,10 +11,10 @@ from Game_Settings import Settings
 import Game_Functions as gf
 
 class maps():
-    def __init__(self, screen, ai_set):
+    def __init__(self, screen, ai_set, image):
         self.screen = screen
         self.screen_rect = screen.get_rect()
-        self.image = pygame.image.load('map.png')
+        self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
         
         self.rect.x = self.screen_rect.centerx
@@ -38,6 +38,8 @@ class Player():
         
         self.grid_x = 0
         self.grid_y = 0
+        self.house_grid_x = 0
+        self.house_grid_y = 0
         
         self.moving_left = False
         self.moving_right = False
@@ -63,58 +65,33 @@ def main_game():
     
     gf.init(screen, ai_set)
     player = Player(screen, ai_set)
-    map_1 = maps(screen, ai_set)
+    map_1 = maps(screen, ai_set, ai_set.map_sheet[0])
+    map_2 = maps(screen, ai_set, ai_set.map_sheet[1])
     
     ######## Music ##########
     pygame.mixer.music.load('Hit_01.mp3')
     
-#    list_tiles(screen_rect, ai_set)
-#    print(ai_set.grid)
-
-#    print(ai_set.grid[1,0]['screen_location'])
-#    print(ai_set.player_frames)
-###    print(ai_set.tile_values)
-
-
     run = True
     while run:
         clock.tick(ai_set.frame_rate)
         gf.check_external_events(ai_set, player, map_1)
         
-        if ai_set.animation_time == 16:    
-            gf.check_events(ai_set, player, map_1)
-            
-        gf.init_movement(ai_set, player, map_1)
-        
-#        print(str(int(clock.get_fps())))
-
-####  Runs Through The Animation And Movement Process  ####
-            #### Automated With No Input ####
-            
-
-
-        ###########################################################    
-        
         screen.fill((0,0,0))
-        map_1.blitme()
         
-#        for i in range(ai_set.set_width):
-#            if i != 0:
-#                pygame.draw.line(screen, (0,0,255), ((screen_rect.width/ai_set.set_width)*i, 0), ((screen_rect.width/ai_set.set_width)*i, screen_rect.height))
-#        
-#        for i in range(ai_set.set_height):
-#            if i != 0:
-#                pygame.draw.line(screen, (0,0,255), (0, (screen_rect.height/ai_set.set_height)*i), (screen_rect.width ,(screen_rect.height/ai_set.set_height)*i))
-#                
-#        for y in range(ai_set.set_height):
-#            for x in range(ai_set.set_width):
-#                pygame.draw.circle(screen, (255, 0, 0), ai_set.grid[x,y]['screen_location'], 2)
+        if ai_set.world:
+            gf.init_movement(ai_set, player, map_1)
+            map_1.blitme()
+            player.blitme()
+            for i in range(len(ai_set.chests)):
+                ai_set.chests[i].update()
+                ai_set.chests[i].animate()
+                ai_set.chests[i].blitme()
         
-        player.blitme()
-        for i in range(len(ai_set.chests)):
-            ai_set.chests[i].update()
-            ai_set.chests[i].blitme()
-        
+        elif ai_set.house:
+            gf.init_movement(ai_set, player, map_2)
+            map_2.blitme()
+            player.blitme()
+    
         pygame.display.flip()
         
 main_game()
