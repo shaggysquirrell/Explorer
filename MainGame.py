d43656fd2cf75927ee9c04cbe569a2f9390b4bc3
Monty <40532678+shaggysquirrell@users.noreply.github.com>
2022-03-07 12:08:08 -0500

    Data_Manager integration

--- MainGame.py
+++ MainGame.py
@@ -9,6 +9,7 @@ import pygame
 
 from Game_Settings import Settings
 import Game_Functions as gf
+import Data_Manager as dm
 
 class maps():
     def __init__(self, screen, ai_set, image):
@@ -61,16 +62,21 @@ def main_game():
     
     ######## Music ##########
     pygame.mixer.music.load('Hit_01.mp3')
+    soun_obj = pygame.mixer.Sound('Open_01.mp3')
+    overworld_msc = pygame.mixer.Sound('Opening.ogg')
+    overworld_msc.play(-1)
+    ######### Data ##########
+    dm.load_data(ai_set, player, map_1, map_2,'explorer_data.json')
     
     run = True
     while run:
         clock.tick(ai_set.frame_rate)
-        gf.check_external_events(ai_set, player, map_1)
-        gf.check_doors(ai_set, player, map_1, map_2)
+        gf.check_external_events(ai_set, player, map_1, map_2)
+        gf.check_doors(ai_set, player, map_1, map_2, soun_obj)
         screen.fill((0,0,0))
         
         if ai_set.world:
-            gf.check_events(ai_set, player, map_1, ai_set.grid, player.grid_x, player.grid_y)
+            gf.check_events(ai_set, player, map_1, map_2, ai_set.grid, player.grid_x, player.grid_y)
             gf.init_movement(ai_set, player, map_1, ai_set.grid, 100, 100)
             map_1.blitme()
             player.blitme()
@@ -80,7 +86,7 @@ def main_game():
                 ai_set.chests[i].blitme()
         
         elif ai_set.house:
-            gf.check_events(ai_set, player, map_2, ai_set.house_grid, player.house_grid_x, player.house_grid_y)
+            gf.check_events(ai_set, player, map_1, map_2, ai_set.house_grid, player.house_grid_x, player.house_grid_y)
             gf.init_movement(ai_set, player, map_2, ai_set.house_grid, 20, 20)
             map_2.blitme()
             player.blitme()
